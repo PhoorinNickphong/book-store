3cb23cba3b959073c99aa50beb860553d61a04b3
ภูรินทร์ นิกพงศ์ <123phoorin@gmail.com>
2022-12-15 22:33:30 +0700

    เพิ่ม title book

--- store-server/src/fixtures.ts
+++ store-server/src/fixtures.ts
@@ -5,12 +5,20 @@ async function loadFixtures(clearData = false){
     console.warn('clearing data')
     await db('book').del()
     await db('category').del()
+    await db('book').del()
   }
 
   await db.batchInsert('category', [
     { id: 1, title: 'Fantasy' },
     { id: 2, title: 'Computer' },
   ])
+  await db.batchInsert('book',[
+    {id: 1,title: 'Harry Potter',price: 560,stock_amount: 10,category_id:1},
+    {id: 2,title: 'The witcher I',price: 399,stock_amount: 6,category_id:1},
+    {id: 3,title: 'Lord of mystery',price: 450,stock_amount: 3,category_id:2}
+])
+
+
 }
 
 export default loadFixtures
