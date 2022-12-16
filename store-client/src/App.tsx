40514be5af1b5164ec8c47a44f5c84032dfa4103
ภูรินทร์ นิกพงศ์ <123phoorin@gmail.com>
2022-12-16 10:15:10 +0700

    แสดงรายละเอียดหนังสือ กับปรับใช้ข้อมูลจาก backend

--- store-client/src/App.tsx
+++ store-client/src/App.tsx
@@ -1,9 +1,11 @@
 import Repo from './repositories'
 import Category from './models/Category';
 import { useEffect, useState } from 'react'
-
+import Book from './models/Book';
+import BookDetail from './components/BookDetail';
 function App() {
   const [categoryList, setCategoryList] = useState<Category[]>([])
+  const [bookList, setBookList] = useState<Book[]>([])
 
   const fetchCategoryList = async () => {
     const result = await Repo.categories.getAll()
@@ -11,17 +13,26 @@ function App() {
       setCategoryList(result)
     }
   }
+  
 
   useEffect(() => {
     fetchCategoryList()
+
   })
 
   return (
     <div>
+      <div>
     {categoryList.map(category => <p key=
     {category.id}> ID : {category.id} Title : 
     {category.title}</p>)}
   </div>
+  <hr />
+      <div>
+        {bookList.map(book => <div key={book.id}><BookDetail {...book}/> <hr /> </div> )}
+      </div>
+    </div>
+
 
   )
 }
