2259043314a02a4a9bb80759e5ec52d9f01f3eb5
ภูรินทร์ นิกพงศ์ <123phoorin@gmail.com>
2022-12-16 10:46:58 +0700

    ปรับโค้ดสามารถกรองรายการหนังสือจาก category ได้

--- store-client/src/App.tsx
+++ store-client/src/App.tsx
@@ -1,41 +1,52 @@
-import Repo from './repositories'
-import Category from './models/Category';
 import { useEffect, useState } from 'react'
-import Book from './models/Book';
 import BookDetail from './components/BookDetail';
-function App() {
+import Book from './models/Book';
+import Category from './models/Category';
+import Repo from './repositories'
+
+
+
+function App(){ 
   const [categoryList, setCategoryList] = useState<Category[]>([])
   const [bookList, setBookList] = useState<Book[]>([])
+  const [filter, setFilter] = useState<string>('')
 
   const fetchCategoryList = async () => {
     const result = await Repo.categories.getAll()
-    if (result) {
-      setCategoryList(result)
-    }
+
   }
-  
-  
+  const fetchBookList = async () => {
+    const result = await Repo.books.getAll({categoryId: filter})
+    if(result){
+      setBookList(result)
+    }
+  }  
 
-  useEffect(() => {
-    fetchCategoryList()
 
-  })
+    useEffect(() => {
+      fetchCategoryList()
+      fetchBookList()
+
+    },[filter])
+  
 
   return (
     <div>
       <div>
-    {categoryList.map(category => <p key=
-    {category.id}> ID : {category.id} Title : 
-    {category.title}</p>)}
-  </div>
-  <hr />
+      <select onChange={e => setFilter(e.target.value)}>
+          <option value="">All</option>
+          {categoryList.map(category => <option key={category.id} value={category.id}>{category.title}</option>)}
+        </select>
+      </div>
+      <hr />
       <div>
         {bookList.map(book => <div key={book.id}><BookDetail {...book}/> <hr /> </div> )}
       </div>
     </div>
-
-
   )
-}
+  }
+
+
+
 
 export default App;
